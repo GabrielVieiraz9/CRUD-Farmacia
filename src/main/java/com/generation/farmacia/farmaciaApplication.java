b08a36261119912d14d08f2c3405bde4eb57dcbb
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-03-26 09:44:00 -0300

    configuração do projeto

---
+++ src/main/java/com/generation/farmacia/farmaciaApplication.java
@@ -0,0 +1,14 @@
+package com.generation.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class farmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(farmaciaApplication.class, args);
+
+	}
+
+}
